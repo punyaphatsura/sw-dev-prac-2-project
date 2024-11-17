6641dcec3ec3d27b9dcae2f44ad2e2c442e50bbc
Punyaphat Surakiatkamjorn <punyaphat.sura@gmail.com>
2024-11-17 14:27:51 +0700

    feat: :sparkles: Change background

--- src/app/(navbar)/layout.tsx
+++ src/app/(navbar)/layout.tsx
@@ -154,7 +154,7 @@ export default function RootLayout({
             </nav>
 
             {/* Main Content */}
-            <main className="mx-auto max-w-7xl bg-white px-4 py-8 sm:px-6 lg:px-8">
+            <main className="mx-auto max-w-7xl px-4 py-8 sm:px-6 lg:px-8">
                 {children}
             </main>
         </div>
